ecba2996a85a074d25f7ff63f4cb7c3a147bd11a
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-08-19 05:46:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(feet){
+  //Write your code here!
+  if (feet <= 400){
+    return 'This one is on me!';
+  }
+else if (feet >= 2000){
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  if (feet > 2500){
+    return 'No can do.';
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  return 'I will gladly take your thirty bucks.';
 }
+}
+function ternaryCheckCity(city){
+  // Write your code here!
+  if (city === 'NYC'){
+    return 'Ok, sounds good.';
+  }
+  else if (city !== 'NYC'){
+    return 'No go.';
+  }
+}
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  if (tip === 'generous'){
+    return 'Thank you so much.';
+  }
+  else if (tip === 'not as generous'){
+    return 'Thank you.';
+  }
+  else{
+    return 'Bye.'
+  }}
